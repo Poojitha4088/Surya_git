16798e4cb6b2b172c97a8925a4fb507c9e95f18e
Poojitha4088 <suryapoojitha3001@gmail.com>
2024-08-01 11:25:45 +0530

    demo

---
+++ Rectanglee.java
@@ -0,0 +1,13 @@
+class Rectangle
+{
+public static void main(String args[])
+{
+int l=5,b=4;
+int Area_rect=l*b;
+int Peri_rect=2*(l+b);
+if(Area_rect>Peri_rect)
+System.out.println("area of rectangle is greater");
+else
+System.out.println("perimeter of rectangle is greater");
+}
+}
